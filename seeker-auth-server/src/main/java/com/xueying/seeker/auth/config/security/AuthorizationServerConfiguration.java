4f7155a17817f5969ba1786b0c8badd60dcb0dde
Allen Allen <apicescn@hotmail.com>
2019-07-24 14:50:56 +0800

    去除不必要的引入类！

--- seeker-auth-server/src/main/java/com/xueying/seeker/auth/config/security/AuthorizationServerConfiguration.java
+++ seeker-auth-server/src/main/java/com/xueying/seeker/auth/config/security/AuthorizationServerConfiguration.java
@@ -30,7 +30,6 @@ import org.springframework.security.oauth2.provider.code.AuthorizationCodeServic
 import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import javax.sql.DataSource;
