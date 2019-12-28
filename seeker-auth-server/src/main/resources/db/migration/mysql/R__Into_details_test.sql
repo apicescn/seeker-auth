e5dd17228900733ae0ce8ccbdb8f7660b3929602
Allen Allen <apicescn@hotmail.com>
2019-12-28 15:56:00 +0800

    因微信不存在用户登陆，故而新增wechat微信公众号端通过client方式来获得Token的参数配置！

--- seeker-auth-server/src/main/resources/db/migration/mysql/R__Into_details_test.sql
+++ seeker-auth-server/src/main/resources/db/migration/mysql/R__Into_details_test.sql
@@ -4,4 +4,7 @@ VALUES ('test', 'xysy-seeker,seeker-web,seeker-auth-server', '$2a$10$7PtegA6OsLb
 INSERT INTO oauth_client_details (client_id,resource_ids,client_secret,scope,authorized_grant_types,web_server_redirect_uri,authorities,access_token_validity,refresh_token_validity,additional_information,autoapprove,bind)
 VALUES ('seeker', 'xysy-seeker,seeker-web,seeker-auth-server', '$2a$10$7PtegA6OsLb402nVZ1C4oOTFvgE42kfnBuDuSGY7JV5plnSH7BMRm', 'read,write', 'client_credentials,password', NULL, 'ADMIN', 100000, NULL, NULL, NULL,0);
 
+INSERT INTO oauth_client_details (client_id,resource_ids,client_secret,scope,authorized_grant_types,web_server_redirect_uri,authorities,access_token_validity,refresh_token_validity,additional_information,autoapprove,bind)
+VALUES ('wechat', 'xysy-seeker,seeker-auth-server,seeker-wechat-admin,seeker-order', '$2a$10$7PtegA6OsLb402nVZ1C4oOTFvgE42kfnBuDuSGY7JV5plnSH7BMRm', 'read,write', 'client_credentials', NULL, NULL, 100000, NULL, NULL, NULL,0);
+
 COMMIT;
